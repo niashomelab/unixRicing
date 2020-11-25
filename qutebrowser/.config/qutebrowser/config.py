c2ad9b18a19bc5ef905f7919dc37a041ef78cc9b
nia <niashomelab@gmail.com>
2020-11-25 14:34:49 +0700

    update qutebrowser config.py

--- qutebrowser/.config/qutebrowser/config.py
+++ qutebrowser/.config/qutebrowser/config.py
@@ -4,3 +4,7 @@ c.url.default_page = "about:blank"
 c.url.start_pages = "about:blank"
 c.url.searchengines = {"DEFAULT": "https://google.com/search?q={}"}
 c.downloads.location.directory = '~'
+c.tabs.last_close = "close"
+c.new_instance_open_target = "window"
+
+c.auto_save.session = True
